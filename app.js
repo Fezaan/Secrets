2f4ee44bc35804cccd9f2abc1c06458ff2c2b333
Fezaan <110244796+Fezaan@users.noreply.github.com>
2023-05-08 13:41:56 +0530

    Login enabled

--- app.js
+++ app.js
@@ -3,21 +3,77 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let userSchema, User;
+
+async function main() {
+  try {
+    await mongoose.connect(
+      "mongodb://127.0.0.1:27017/secretDB?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.8.0",
+      { useNewUrlParser: true }
+    );
+    console.log("Connected");
+
+    userSchema = mongoose.Schema({
+      email: String,
+      password: String,
+    });
+
+    User = new mongoose.model("User", userSchema);
+  } catch (err) {
+    console.log(err);
+  }
+}
+main();
+
 app.get("/", async (req, res) => {
   res.render("home");
 });
-app.get("/login", async (req, res) => {
-  res.render("login");
-});
-app.get("/register", async (req, res) => {
-  res.render("register");
-});
+app
+  .route("/login")
+  .get(async (req, res) => {
+    res.render("login");
+  })
+  .post(async (req, res) => {
+    let userName = req.body.username;
+    let pass = req.body.password;
+    let found;
+    try {
+      found = await User.findOne({ email: userName });
+    } catch (err) {
+      res.send(err);
+    }
+    if (found) {
+      if (found.password === pass) {
+        res.render("secrets");
+      }
+    } else {
+      res.redirect("/register");
+    }
+  });
+app
+  .route("/register")
+  .get(async (req, res) => {
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    let newUser = new User({
+      email: req.body.username,
+      password: req.body.password,
+    });
+    try {
+      await newUser.save();
+      res.render("secrets");
+    } catch (err) {
+      res.send(err);
+    }
+  });
 
 app.listen("3000", () => {
   console.log("Server startrd at port 3000");
